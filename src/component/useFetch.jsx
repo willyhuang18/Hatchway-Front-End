6243786330eabb3205513d25b8d43ef571dae5bb
Willy Huang <willyhuang18@gmail.com>
2022-05-19 20:22:13 -0700

    creating usefetch custom hook

---
+++ src/component/useFetch.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function useFetch(url){
+
+}
+
+
+export default useFetch;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import App from "./App";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
