15269fda71190acf2bf4e2e8dad4086641c76909
Willy Huang <willyhuang18@gmail.com>
2022-05-19 20:38:56 -0700

    testing the api

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import useFetch from "./useFetch";
 
 function App() {
 
+  const {data, loading, error} = useFetch("https://v2.jokeapi.dev/joke/Any");
+
+  if(loading) return <h1> Loading ....</h1>
+
+  if(error) console.log(error);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <h1>{data?.setup} : {data?.delivery}</h1>
     </div>
   );
 }
