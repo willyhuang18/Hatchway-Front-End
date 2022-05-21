9583ebe22e5c4511c844a78e2f96cdf7d6b01a23
Willy Huang <willyhuang18@gmail.com>
2022-05-20 22:06:53 -0700

    print out every student in the API

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 function Fetch(){
     const [posts, setPosts] = useState([]);
+    const [open, setOpen]= useState(true);
 
     useEffect(()=>{
       axios.get('https://api.hatchways.io/assessment/students')
@@ -19,18 +20,24 @@ function Fetch(){
       <div className="relative top-64  rounded-lg shadow-lg container h-80 ">
         <div className=" h-full w-full col-auto p-6  overflow-auto  ">
           {posts.map(post => (
-              <div className="flex ">
+              <div className="flex flex-warp gap-10 ">
                 <div>
                   <img src={post.pic} className="rounded-full h-24 w-24 object-contain border-black border-2 p-1 mr-3" alt="icon"/>
                 </div>
-                <ul>
-                  <li className="text-3xl font-bold uppercase " key={post.id}>{post.firstName} {post.lastName} </li>
-                  <li>Email: </li>
-                  <li>Company: </li>
-                  <li>Skill: </li>
-                  <li>Grade: </li>
-
-                </ul>
+                <div>
+                  <p className="text-3xl font-bold uppercase " key={post.id}>{post.firstName} {post.lastName} </p>
+                  <p>Email: {post.email}</p>
+                  <p>Company: {post.company} </p>
+                  <p>Skill: {post.skill} </p>
+                  <p>Average: {post.grades.reduce((a,b) => Number(a) + Number(b), 0) / post.grades.length} </p>
+                    <ul>
+                    {!open ? 
+                    post.grades.map((grade, index) =>(
+                      <li key={index}>Test{post.grades.indexOf(grade)+ 1 +":"}{grade}</li>
+                    )) : " "
+                  }
+                  </ul>
+                </div>
               </div>
             ))
           }
